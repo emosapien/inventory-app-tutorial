7368683781033b1c2b48dbf1d1531970a997155c
emosapien <le.emosapien@gmail.com>
2020-11-29 14:35:04 -0800

    tada: initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Model A SQL Database
+
+Entities
+* [ ] User
+* [ ] Item
+* [ ] Item Type
+* [ ] Location
+* [ ] Manufacturer
+* [ ] Address
+* [ ] 
+* [ ] 
+* [ ] 
+* [ ] 
+* [ ] 
+* [ ] 
+* [ ] 
---
+++ backend/db/migrations/20201128225733_initial.js
@@ -0,0 +1,112 @@
+// const knex = require('knex');
+
+const tableNames = require('../../src/constants/tableNames');
+
+function addDefaultColumns(table) {
+  table.timestamps(false, true);
+  table.datetime('deleted_at');
+}
+
+function createNameTable(knex, table_name) {
+  return knex.schema.createTable(table_name, (table) => {
+    table.increments().notNullable();
+    table.string('name').notNullable().unique();
+    addDefaultColumns(table);
+  });
+}
+
+function references(table, tableName) {
+  table
+    .integer(`${tableName}_id`)
+    .unsigned()
+    .references('id')
+    .inTable(tableName)
+    .onDelete('cascade');
+}
+
+function url(table, columnName) {
+  table.string(columnName, 2000);
+}
+
+function email(table, columnName) {
+  return table.string(columnName, 254);
+}
+
+/**
+ * @param {knex} knex
+ */
+
+exports.up = async (knex) => {
+  await Promise.all([
+    knex.schema.createTable(tableNames.user, (table) => {
+      table.increments().notNullable();
+      email(table, 'email').notNullable().unique();
+      table.string('name').notNullable();
+      table.string('password', 127).notNullable();
+      table.datetime('last_login');
+      addDefaultColumns(table);
+    }),
+
+    createNameTable(knex, tableNames.item_type),
+    createNameTable(knex, tableNames.country),
+    createNameTable(knex, tableNames.state),
+    createNameTable(knex, tableNames.shape),
+
+    knex.schema.createTable(tableNames.location, (table) => {
+      table.increments().notNullable();
+      table.string('name').notNullable();
+      table.string('description', 1000);
+      url(table, 'image_url');
+      addDefaultColumns(table);
+    }),
+  ]);
+
+  await knex.schema.createTable(tableNames.address, (table) => {
+    table.increments().notNullable();
+    table.string('street_address_1', 50).notNullable();
+    table.string('street_address_2', 50);
+    table.string('city', 50).notNullable();
+    table.string('zipcode', 15).notNullable();
+    table.double('latitude').notNullable();
+    table.double('longitude').notNullable();
+    references(table, 'state');
+    references(table, 'country');
+    addDefaultColumns(table);
+    table.unique([
+      'street_address_1',
+      'street_address_2',
+      'city',
+      'zipcode',
+      'country_id',
+      'state_id',
+    ]);
+  });
+
+  await knex.schema.createTable(tableNames.company, (table) => {
+    table.increments().notNullable();
+    table.string('name').notNullable();
+    url(table, 'logo_url');
+    table.string('description', 1000);
+    url(table, 'website_url');
+    email(table, 'email');
+    references(table, 'address');
+    addDefaultColumns(table);
+  });
+};
+
+exports.down = async (knex) => {
+  await Promise.all(
+    [
+      tableNames.company,
+      tableNames.address,
+      tableNames.user,
+      tableNames.item_type,
+      tableNames.country,
+      tableNames.shape,
+      tableNames.country,
+      tableNames.location,
+      tableNames.state,
+
+    ].map((tableName) => knex.schema.dropTableIfExists(tableName)),
+  );
+};
